5cf8d13f236ae4a5b76b0dddb08e6fd3c7574156
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-04-26 19:20:58 -0300

    create new game button

--- src/mainMenu.js
+++ src/mainMenu.js
@@ -1,8 +1,16 @@
 export default function mainMenu(){
     const gameContent = document.querySelector("#gameContent");
     gameContent.classList.add("main-menu")
+
     const mainTitle = document.createElement("h1");
     mainTitle.innerHTML = "JavaScript Legends";
-    gameContent.appendChild(mainTitle);
     mainTitle.classList.add("main-menu-title")
+    gameContent.appendChild(mainTitle);
+
+    const newGameButton = document.createElement("div");
+    newGameButton.innerHTML = "NIGGA GAME";
+    newGameButton.classList.add("new-game-button");
+    gameContent.appendChild(newGameButton);
+    
+
 }
